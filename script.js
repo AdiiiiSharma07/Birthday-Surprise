db4b09082a65774998a15fcb6a75f821dc4018cc
AdiiiiSharma07 <sharmaadi9324@gmail.com>
2026-07-15 21:04:43 +0530

    Refactor surprise opening and heart animation
    Refactor surprise opening logic and heart creation.

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
+function openSurprise(){
 
-    const startBtn = document.getElementById("startBtn");
-    const loadingScreen = document.querySelector(".loading-screen");
-    const birthdayPage = document.querySelector(".birthday-page");
+document.querySelector(".one").style.display="none";
 
-    startBtn.addEventListener("click", () => {
+document.querySelector(".two").style.display="flex";
 
-        loadingScreen.style.opacity = "0";
+}
 
-        setTimeout(() => {
-            loadingScreen.style.display = "none";
-            birthdayPage.style.display = "block";
 
-            birthdayPage.animate([
-                { opacity: 0, transform: "translateY(40px)" },
-                { opacity: 1, transform: "translateY(0)" }
-            ], {
-                duration: 1200,
-                fill: "forwards"
-            });
+function createHeart(){
 
-        }, 700);
+let heart=document.createElement("div");
 
-    });
+heart.className="heart";
 
-});
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.animationDuration=
+(3+Math.random()*4)+"s";
+
+
+document.body.appendChild(heart);
+
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+}
+
+
+setInterval(createHeart,300);
